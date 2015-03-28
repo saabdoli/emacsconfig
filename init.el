2fb2da9173ad4e97df8e33264a5574aeea02e8fd
saabdoli <saabdoli@gmail.com>
2015-03-28 11:54:47 -0500

    Added zenburn and solarized themes to emacs themes, default theme is now Solarized-Dark

--- init.el
+++ init.el
@@ -4,12 +4,14 @@
 (package-initialize)
 (require 'evil)
 (evil-mode 1)
+(load-theme 'solarized-dark t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(evil-auto-indent t)
  '(evil-default-cursor (quote (t "black"))))
 (custom-set-faces
